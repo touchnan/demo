b48efff0cc2ffce3f363764698182dfde04b4293
touchnan <88052350@qq.com>
2018-08-30 15:57:19 +0800

    spirng cloud stream kafka

---
+++ demo-spring-cloud/spring-cloud-gradle/spring-cloud-stream-kafka/src/main/java/cn/touch/demo/cloud/stream/springcloudstreamkafka/SpringCloudStreamKafkaApplication.java
@@ -0,0 +1,34 @@
+package cn.touch.demo.cloud.stream.springcloudstreamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding(Sink.class)
+public class SpringCloudStreamKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamKafkaApplication.class, args);
+	}
+
+	@StreamListener(Sink.INPUT)
+	public void handle(Person person) {
+		System.out.println("Received: " + person);
+	}
+
+	public static class Person {
+		private String name;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String toString() {
+			return this.name;
+		}
+	}
+}
