057d55daaacea8c6151562dec6776a6ed87a9250
touchnan <88052350@qq.com>
2018-11-16 19:28:47 +0800

    mongo simple example

--- demo-spring-boot/src/main/java/cn/touch/demo/boot/demospringboot/mongo/DemoMongoRepository.java
+++ demo-spring-boot/src/main/java/cn/touch/demo/boot/demospringboot/mongo/DemoMongoRepository.java
@@ -1,5 +1,8 @@
 package cn.touch.demo.boot.demospringboot.mongo;
 
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
@@ -8,6 +11,19 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  *
  * 可以不用注解 @Repository,自动实例化,
  * 如果需要定制自己的个性化接口，可以在实现类中extends SimpleMongoRepository
+ *
+ * 也可以直接在接口中@Query
  */
 public interface DemoMongoRepository extends MongoRepository<MongoPerson,String> {
+
+
+    /**
+     * https://docs.spring.io/spring-data/mongodb/docs/2.0.11.RELEASE/reference/html/#mongodb-template-update
+     */
+//    List<MongoPerson> findByLastname(String lastname);
+
+    //extends PagingAndSortingRepository<MongoPerson, String>
+//    Page<MongoPerson> findByFirstname(String firstname, Pageable pageable);
+//    @Query(value="{ 'firstname' : ?0 }", fields="{ 'firstname' : 1, 'lastname' : 1}")
+//    List<MongoPerson> findByThePersonsFirstname(String firstname);
 }
