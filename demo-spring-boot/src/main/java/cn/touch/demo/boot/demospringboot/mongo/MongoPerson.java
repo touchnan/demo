bb9c4f6fabb4284918c2697301bd19d0e2549989
touchnan <88052350@qq.com>
2018-11-27 15:25:03 +0800

    spring data mongo annotation

--- demo-spring-boot/src/main/java/cn/touch/demo/boot/demospringboot/mongo/MongoPerson.java
+++ demo-spring-boot/src/main/java/cn/touch/demo/boot/demospringboot/mongo/MongoPerson.java
@@ -1,11 +1,20 @@
 package cn.touch.demo.boot.demospringboot.mongo;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
  * Created by chengqiang.han on 2018/10/29.
  */
+@Document(collection = "person")
+//@TypeAlias("")  //对象间引用时可以用别名来代替类名
 public class MongoPerson {
     @Id
     private String id;
+
+    @Field("name")
+    @Indexed
+    private String name;
 }
